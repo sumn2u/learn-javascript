6360126afc3134c9a7317a4662f8ce5fdd3a0531
suman <suman.kunwar@daliaresearch.com>
2023-04-15 14:13:16 -0500

    fix: 🐛 share link of twitter

--- _book/gitbook/gitbook-plugin-sharing/buttons.js
+++ _book/gitbook/gitbook-plugin-sharing/buttons.js
@@ -1,56 +1,36 @@
 require(['gitbook', 'jquery'], function(gitbook, $) {
+    function site(label, icon, link) {
+        return {
+            label: label,
+            icon: 'fa fa-' + icon,
+            onClick: function (e) {
+                e.preventDefault();
+                window.open(link);
+            }
+        };
+    }
+
+    var url = encodeURIComponent(location.href);
+    var title = encodeURIComponent(document.title);
+
     var SITES = {
-        'facebook': {
-            'label': 'Facebook',
-            'icon': 'fa fa-facebook',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open('http://www.facebook.com/sharer/sharer.php?s=100&p[url]='+encodeURIComponent(location.href));
-            }
-        },
-        'twitter': {
-            'label': 'Twitter',
-            'icon': 'fa fa-twitter',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open('http://twitter.com/home?status='+encodeURIComponent(document.title+' '+location.href));
-            }
-        },
-        'google': {
-            'label': 'Google+',
-            'icon': 'fa fa-google-plus',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open('https://plus.google.com/share?url='+encodeURIComponent(location.href));
-            }
-        },
-        'weibo': {
-            'label': 'Weibo',
-            'icon': 'fa fa-weibo',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open('http://service.weibo.com/share/share.php?content=utf-8&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title));
-            }
-        },
-        'instapaper': {
-            'label': 'Instapaper',
-            'icon': 'fa fa-instapaper',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open('http://www.instapaper.com/text?u='+encodeURIComponent(location.href));
-            }
-        },
-        'vk': {
-            'label': 'VK',
-            'icon': 'fa fa-vk',
-            'onClick': function(e) {
-                e.preventDefault();
-                window.open('http://vkontakte.ru/share.php?url='+encodeURIComponent(location.href));
-            }
-        }
+        douban: site('豆瓣', 'share', 'http://shuo.douban.com/!service/share?href=' + url + '&name=' + title),
+        facebook: site('Facebook', 'facebook', 'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=' + url+ '&title=' + title),
+        google: site('Google+', 'google-plus', 'https://plus.google.com/share?url=' + url),
+        instapaper: site('instapaper', 'instapaper', 'http://www.instapaper.com/text?u=' + url),
+        line: site('LINE', 'comment', 'http://line.me/R/msg/text/?' + title + ' ' + url),
+        linkedin: site('Linkedin', 'linkedin', 'https://www.linkedin.com/shareArticle?mini=true&url=' + url+ '&title=' + title),
+        messenger: site('Facebook Messenger', 'commenting', 'fb-messenger://share?link=' + url),
+        pocket: site('Pocket', 'get-pocket', 'https://getpocket.com/save?url=' + url + '&title=' + title),
+        qq: site('QQ', 'qq', 'http://connect.qq.com/widget/shareqq/index.html?url=' + url + '&title=' + title),
+        qzone: site('QQ空间', 'star', 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=' + url + '&title=' + title),
+        stumbleupon: site('StumbleUpon', 'stumbleupon', 'http://www.stumbleupon.com/submit?url=' + url + '&title=' + title),
+        twitter: site('Twitter', 'twitter', 'https://twitter.com/intent/tweet?url=' + url + '&text=' + title),
+        viber: site('Viber', 'volume-control-phone', 'viber://forward?text='+ url + ' ' + title),
+        vk: site('VK', 'vk', 'http://vkontakte.ru/share.php?url=' + url),
+        weibo: site('新浪微博', 'weibo', 'http://service.weibo.com/share/share.php?content=utf-8&url=' + url + '&title=' + title),
+        whatsapp: site('WhatsApp', 'whatsapp', 'whatsapp://send?text='+ url + ' ' + title),
     };
-
-
 
     gitbook.events.bind('start', function(e, config) {
         var opts = config.sharing;
